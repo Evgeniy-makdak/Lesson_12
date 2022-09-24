4bdf350b9579e4395da015cd2a6803c254fdc784
Evgeniy Acteck <makdak23@mail.ru>
2022-09-24 10:37:10 +0300

    first version

---
+++ logistics.py
@@ -0,0 +1,54 @@
+class Storage:
+    def __init__(self, items: dict, capacity: int):
+        self.items = items
+        self.capacity = capacity
+
+    def add(self, product_name: str, count: int):
+        if product_name in self.items:
+            self.items[product_name] += count
+        else:
+            self.items[product_name] = count
+
+    def remove(self, product_name: str, count: int):
+        if product_name in self.items and count <= self.items[product_name]:
+            self.items[product_name] -= count
+            return True
+
+    def get_free_space(self):
+        free_space = self.capacity - sum(self.items.values())
+        return free_space
+
+    def get_items(self):
+        return self.items
+
+    def get_unique_items_count(self):
+        return len(self.items)
+
+
+class Store(Storage):
+    def __init__(self, items: dict, capacity=100):
+        super().__init__(items, capacity)
+
+    def add(self, product_name: str, count: int):
+        if count <= self.get_free_space():
+            super().add(product_name, count)
+
+
+class Shop(Store):
+    def __init__(self, items: dict, capacity=20):
+        super().__init__(items, capacity)
+
+    def add(self, product_name: str, count: int):
+        if product_name in self.get_items() or self.get_unique_items_count() < 5:
+            super().add(product_name, count)
+
+
+class Request:
+    def __init__(self, from_1: str, to: str, amount: int, product: str):
+        self.from_1 = from_1
+        self.to = to
+        self.amount = amount
+        self.product = product
+
+    def __repr__(self):
+        return f'Доставить из {self.from_1} в {self.to} {self.amount}, {self.product}'
---
+++ main.py
@@ -0,0 +1,32 @@
+from logistics import *
+import json
+
+with open('items.json', 'r', encoding='utf-8') as file:
+    item_list = json.load(file)
+it = item_list[0]
+
+
+def main():
+    while True:
+        store = Store(it)
+        shop = Shop({})
+        print(f'На складе находится следующая продукция: {store.get_items()}')
+        print(f'Свободных мест на складе: {store.get_free_space()}')
+        print(f'Уникальных продуктов на складе: {store.get_unique_items_count()}')
+
+        from_1 = "store"
+        to = "shop"
+
+        product = input("Введите наименование продукции для перемещения: ")
+        amount = int(input("Введите количество товара: "))
+
+        print(f"Курьер забирает {amount} {product} из {from_1} и отвозит в {to}")
+        request = Request(from_1, to, amount, product)
+        store.remove(count=request.amount, product_name=request.product)
+        shop.get_free_space()
+        shop.add(count=request.amount, product_name=request.product)
+        print(f'На складе хранится {store.get_items()}')
+        print(f'В магазине хранится {shop.get_items()}')
+
+
+main()
